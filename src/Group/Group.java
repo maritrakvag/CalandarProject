a1b9882c91559865d38a3405fc2eadf9c797db68
astridnd <astridnd@stud.ntnu.no>
2015-02-24 09:20:25 +0100

    Endra group
    Marit <3

--- src/Group/Group.java
+++ src/Group/Group.java
@@ -1,5 +1,24 @@
 package Group;
 
-public class Group {
 
+public class Group {
+ private ArrayList<User> users;
+public Group(){
+  users = new ArrayList<User> users();
+  
 }
+public void addMember(User user){
+  users.add(user);
+}
+public void deleteMember(User user){
+  users.remove(user);
+}
+public ArrayList<User> getMembers() {
+  return users;
+}
+
+public boolean containsUser(User user) {
+  return users.contains(user);
+}
+}
+
