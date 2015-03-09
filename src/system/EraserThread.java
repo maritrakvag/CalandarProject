275a0ab572fc2f718c7b7257a502d858b97b5c2b
alandsem <amlandsem@gmail.com>
2015-03-09 13:33:20 +0100

    Update EraserThread.java

--- src/system/EraserThread.java
+++ src/system/EraserThread.java
@@ -1,5 +1,37 @@
 package system;
 
-public class EraserThread {
+import java.io.*;
+
+public class EraserThread implements Runnable {
+  private boolean stop;
+
+   /**
+    *@param The prompt displayed to the user
+    */
+   public EraserThread(String prompt) {
+       System.out.print(prompt);
+   }
+
+   /**
+    * Begin masking...display asterisks (*)
+    */
+   public void run () {
+      stop = true;
+      while (stop) {
+         System.out.print("\010*");
+     try {
+        Thread.currentThread().sleep(1);
+         } catch(InterruptedException ie) {
+            ie.printStackTrace();
+         }
+      }
+   }
+
+   /**
+    * Instruct the thread to stop masking
+    */
+   public void stopMasking() {
+      this.stop = false;
+   }
 
 }
